3f6e343a8f21398cc57ed39eb0015c7a5b1d54ed
Arturo Guzman <gab_r2ro@hotmail.com>
2022-09-26 10:53:58 -0500

    feat: uso de script externo

---
+++ script.js
@@ -0,0 +1,7 @@
+console.log("Saoko");
+document.getElementById("generation").style.color = "grey";
+
+function colorTitulo(){
+  console.log("Pulso el boton generation");
+  document.getElementById("generation").style.color = "red";
+}
